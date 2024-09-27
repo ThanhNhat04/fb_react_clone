03fe7d946578510f7e0cbc21096a780245241614
ThanhNhat04 <thanhnhat0411@gmail.com>
2024-09-27 14:02:34 +0700

    27/9

---
+++ src/pages/home/index.js
@@ -0,0 +1,19 @@
+import Header from "../../components/header";
+
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            {/* <main /> */}
+        </div>
+    );
+}
+
+// function main() {
+//     return (
+//         <div>
+//             <h1>hello</h1>
+//         </div>
+//     )
+// }
